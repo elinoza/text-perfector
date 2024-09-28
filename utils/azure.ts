9589d14e84786ab1851e1acc4a1e23a152fe041d
elinoza <hillcakmak@gmail.com>
2024-09-29 01:58:48 +0300

    azure resource name

--- utils/azure.ts
+++ utils/azure.ts
@@ -3,7 +3,7 @@
 import { createAzure } from "@ai-sdk/azure";
 
 const azure = createAzure({
-  resourceName: "cloock-ai", // Azure resource name
+  resourceName: process.env.AZURE_OPENAI_RESOURCE_NAME, // Azure resource name
   apiKey: process.env.AZURE_OPENAI_API_KEY,
 });
 export default azure;
