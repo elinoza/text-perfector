36d1289ea94452a2cb42c33cdb9ed8564a4f2bc8
elinoza <hillcakmak@gmail.com>
2024-10-02 22:01:23 +0300

    dark class deleted

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="text-neutral-600 dark:text-neutral-300 flex items-center justify-end">
+    <div className="text-neutral-600 flex items-center justify-end">
       <span className="ml-2">
         <FaGithub />
       </span>{" "}
