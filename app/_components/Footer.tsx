e3e4bb193f43f7262576d1b1285d4af773b55035
elinoza <hillcakmak@gmail.com>
2024-10-08 17:39:43 +0300

    github

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,7 +3,11 @@ import { FaGithub } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className="text-neutral-600 flex items-center justify-end">
-      <a href="https://github.com/elinoza" target="_blank" className="ml-2">
+      <a
+        href="https://github.com/elinoza/text-perfector"
+        target="_blank"
+        className="ml-2"
+      >
         <FaGithub />
       </a>{" "}
     </div>
